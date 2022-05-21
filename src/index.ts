0af822b8c938625d1e3945b2ef1717e9b07c9de5
erzetid <fahrizalm14@gmail.com>
2022-05-21 16:40:06 +0700

    Register auth api

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+import AuthApi from '@service/auth/api';
 import UserApi from '@service/user/api';
 
 export const userApi = UserApi;
+export const authApi = AuthApi;
 
+authApi.start();
 userApi.start();
