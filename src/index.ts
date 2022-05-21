ad9361908af3b88d4e219a16fd5f111b71c8303d
erzetid <fahrizalm14@gmail.com>
2022-05-21 14:15:48 +0700

    Run user api service

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 fahrizalm14
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+import UserApi from '@service/user/api';
+
+export const userApi = UserApi;
+
+userApi.start();
